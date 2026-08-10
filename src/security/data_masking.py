e86ceb0670082e8601bf1f2dae01b25a0b09165e
Mahsun Yalçın <153347691+DtScntst1@users.noreply.github.com>
2026-08-10 16:18:15 +0300

    Remove Python/SQL script for issue 21

--- src/security/data_masking.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-def mask_ip(ip_address):
-    return hashlib.sha256(ip_address.encode('utf-8')).hexdigest()[:8] + '***'
-
-def mask_pii(record):
-    record['email'] = record['email'][0] + '***@' + record['email'].split('@')[1]
-    return record
