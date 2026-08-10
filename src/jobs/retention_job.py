d04175cf73b296361e9fe795691bf4351dfe6c88
Mahsun Yalçın <153347691+DtScntst1@users.noreply.github.com>
2026-08-10 16:18:12 +0300

    Remove Python/SQL script for issue 22

--- src/jobs/retention_job.py
+++
@@ -1,6 +0,0 @@
-import datetime
-
-def nullify_old_gps_data(db_conn, retention_days=30):
-    cutoff_date = datetime.date.today() - datetime.timedelta(days=retention_days)
-    db_conn.execute('UPDATE locations SET lat=NULL, lng=NULL WHERE created_at < ?', (cutoff_date,))
-    db_conn.commit()
