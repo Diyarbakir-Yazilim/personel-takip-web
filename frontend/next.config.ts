44908d4490c5cf272d83df7e4d4bca4f86801838
vahap akgul <191167319+vhpakgl@users.noreply.github.com>
2026-08-12 13:51:04 +0300

    feat: implement HTML5 QR scanner UI

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  allowedDevOrigins: [
+    "viewing-played-highs-nano.trycloudflare.com",
+  ],
 };
 
 export default nextConfig;
