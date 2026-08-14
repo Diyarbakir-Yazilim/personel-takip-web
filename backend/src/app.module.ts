4e1abf5efb867efd1c38afcdecf2009132903ef4
Mahsun Yalçın <153347691+DtScntst1@users.noreply.github.com>
2026-08-14 09:22:40 +0300

    Register RetentionService in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RetentionService } from './modules/retention/retention.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, RetentionService],
 })
 export class AppModule {}
